c3550eb94dbbe9067abc2679a844688e81b9efd5
alexiso12 <45578529+alexiso12@users.noreply.github.com>
2018-12-10 19:22:28 -0500

    Add files via upload

---
+++ Online Library/inc/data.php
@@ -0,0 +1,163 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "Up",
+	"img" => "img/media/up.jpg",
+    "genre" => "Poetry",
+    "format" => "Hardcover",
+    "year" => 2003,
+    "category" => "Books",
+    "authors" => [
+        "Shel Silversten"
+    ],
+    "publisher" => "Harpercollins Childrens Books",
+    "isbn" => '0060513098'
+];
+$catalog[102] = [
+    "title" => "Ramona Quimby Age 8",
+    "img" => "img/media/ramona.jpg",
+    "genre" => "Fiction",
+    "format" => "Paperback",
+    "year" => 1982,
+    "category" => "Books",
+    "authors" => [
+        "Beverly Cleary"
+    ],
+    "publisher" => "Yearling",
+    "isbn" => '0440800463'
+];
+$catalog[103] = [
+    "title" => "Goodnight Moon",
+    "img" => "img/media/goodnightmoon.jpg",
+    "genre" => "Classic",
+    "format" => "Hardcover",
+    "year" => 1999,
+    "category" => "Books",
+    "authors" => [
+			"Margaret Wise Brown",
+			"Clement Hurd"
+    ],
+    "publisher" => "HarperCollins",
+    "isbn" => '060775858'
+];
+$catalog[104] = [
+    "title" => "Love that Dog",
+    "img" => "img/media/lovethatdog.jpg",
+    "genre" => "Fiction",
+    "format" => "Paperback",
+    "year" => 2001,
+    "category" => "Books",
+    "authors" => [
+        "Sharon Creech"
+    ],
+    "publisher" => "HarperCollins",
+    "isbn" => '0064409597'
+];
+//Movies
+$catalog[201] = [
+    "title" => "Napoleon Dynamite",
+    "img" => "img/media/nd.jpg",
+    "genre" => "Comedy",
+    "format" => "DVD",
+    "year" => 2004,
+    "category" => "Movies",
+    "director" => "Jared Hess",
+    "writers" => [
+        "Jared Hess",
+        "Jerusha Hess"
+    ],
+    "stars" => [
+        "Jon Heder",
+        "Efren Ramirez",
+        "Jon Gries"
+    ]
+];
+$catalog[202] = [
+    "title" => "Space Jam",
+    "img" => "img/media/spacejam.jpg",
+    "genre" => "Animation,Adventure,Comedy",
+    "format" => "Blu-ray",
+    "year" => 1996,
+    "category" => "Movies",
+    "director" => "Joe Pytka",
+    "writers" => [
+        "Leo Benvenuti"
+    ],
+    "stars" => [
+        "Michael Jordan",
+        "Wayne Knight",
+        "Theresa Randle"
+    ]
+];
+$catalog[203] = [
+    "title" => "Scarface",
+    "img" => "img/media/scarface.jpeg",
+    "genre" => "Action,Thriller",
+    "format" => "Blu-ray",
+    "year" => 1983,
+    "category" => "Movies",
+    "director" => "Brian De Palma",
+    "writers" => [
+        "Oliver Stone"
+    ],
+    "stars" => [
+        "Al Pacino",
+        "Michelle Pfeiffer",
+        "Steven Bauerr"
+    ]
+];
+$catalog[204] = [
+    "title" => "Fantastic Mr. Fox",
+    "img" => "img/media/fantasticmrfox.jpg",
+    "genre" => "Animation,Adventure,Comedy",
+    "format" => "DVD",
+    "year" => 2009,
+    "category" => "Movies",
+    "director" => "Wes Anderson",
+    "writers" => [
+        "Wes Anderson"
+    ],
+    "stars" => [
+        "George Clooney",
+        "Meryl Streep",
+        "Bill Murray"
+    ]
+];
+//Music
+$catalog[301] = [
+    "title" => "Kush and Orange Juice",
+    "img" => "img/media/kushandoj.jpg",
+    "genre" => "Rap",
+    "format" => "CD",
+    "year" => 2010,
+    "category" => "Music",
+    "artist" => "Wiz Khalifa"
+];
+$catalog[302] = [
+    "title" => "Man on the Moon 2",
+    "img" => "img/media/manonthemoon.jpg",
+    "genre" => "Rap",
+    "format" => "CD",
+    "year" => 2010,
+    "category" => "Music",
+    "artist" => "Kid Cudi"
+];
+$catalog[303] = [
+    "title" => "KALA",
+    "img" => "img/media/kala.jpg",
+    "genre" => "World Music",
+    "format" => "CD",
+    "year" => 2007,
+    "category" => "Music",
+    "artist" => "M.I.A."
+];
+$catalog[304] = [
+    "title" => "Seeing Sounds",
+    "img" => "img/media/seeingsounds.jpg",
+    "genre" => "Alternative",
+    "format" => "CD",
+    "year" => 2008,
+    "category" => "Music",
+    "artist" => "NERD"
+];
